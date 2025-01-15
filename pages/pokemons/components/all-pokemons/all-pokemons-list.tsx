5de95e2660f2a1d619a9f458c4d3f83fccf00291
Wilmar Florez <wilmar.florez@elenas.la>
2025-01-15 10:35:00 -0500

    add loading state

--- pages/pokemons/components/all-pokemons/all-pokemons-list.tsx
+++ pages/pokemons/components/all-pokemons/all-pokemons-list.tsx
@@ -8,7 +8,14 @@ import { PokemonCard } from '../pokemon-card/pokemon-card';
 import { PokemonsGrid } from '../pokemons-grid/pokemons-grid';
 
 export const AllPokemonList = () => {
-  const { data, isLoading, hasNextPage, fetchNextPage } = useInfinitePokemons();
+  const {
+    data,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isError,
+    isFetchingNextPage,
+  } = useInfinitePokemons();
   const { isIntersecting, ref } = useIsVisible();
 
   useEffect(() => {
@@ -16,6 +23,26 @@ export const AllPokemonList = () => {
       fetchNextPage();
     }
   }, [isIntersecting, hasNextPage, fetchNextPage]);
+
+  if (!data || isError || isLoading)
+    return (
+      <div>
+        {!data && !isLoading ? (
+          <p className="text-center mt-12 text-lg">
+            No hemos encontrado resultados. Por favor, ingresa un término válido
+            de búsqueda. Ejemplo: fire
+          </p>
+        ) : null}
+        {isError ? (
+          <p className="text-center mt-12 text-lg">
+            Ah ocurrrido un error, intentalo mas tarde
+          </p>
+        ) : null}
+        {isLoading ? (
+          <p className="text-center mt-12 text-lg">Cargando...</p>
+        ) : null}
+      </div>
+    );
 
   return (
     <div>
@@ -27,6 +54,7 @@ export const AllPokemonList = () => {
           ))}
         {hasNextPage ? <div ref={ref} className="my-8 h-2" /> : null}
       </PokemonsGrid>
+      {isFetchingNextPage ? <p>Cargando...</p> : null}
     </div>
   );
 };
--- pages/pokemons/hooks/useInfinitePokemons.ts
+++ pages/pokemons/hooks/useInfinitePokemons.ts
@@ -3,8 +3,21 @@ import { PokemonsResponseApi } from '@pokemon/types/pokemons';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 export function useInfinitePokemons() {
-  const { data, isLoading, hasNextPage, fetchNextPage } =
-    useInfiniteQuery<PokemonsResponseApi>(getPaginatedPokemonsOptions());
+  const {
+    data,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isError,
+    isFetchingNextPage,
+  } = useInfiniteQuery<PokemonsResponseApi>(getPaginatedPokemonsOptions());
 
-  return { data, isLoading, hasNextPage, fetchNextPage };
+  return {
+    data,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isError,
+    isFetchingNextPage,
+  };
 }
