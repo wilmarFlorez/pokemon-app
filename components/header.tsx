29eefa2927b63397c7396ccf32c6ce168c94e3fe
Wilmar Florez <wilmar.florez@elenas.la>
2025-01-15 09:36:29 -0500

    fix header

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export const Header = () => {
   return (
-    <header className="bg-red-600 p-2 flex justify-center fixed top-0 left-0 w-full">
+    <header className="bg-red-600 p-2 flex justify-center fixed top-0 left-0 w-full z-20">
       <Image src="/images/logo.png" alt="Logo pokemon" width={66} height={40} />
     </header>
   );
