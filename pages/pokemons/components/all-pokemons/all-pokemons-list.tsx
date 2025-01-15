821f48ac8fc0c31cea7daccd51ea312d11ba6c1f
Wilmar Florez <wilmar.florez@elenas.la>
2025-01-14 21:24:53 -0500

    add all pokemon list

---
+++ pages/pokemons/components/all-pokemons/all-pokemons-list.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useInfinitePokemons } from '../../hooks/useInfinitePokemons';
+import { useIsVisible } from '@pokemon/hooks/useIntersectionObserver';
+import { PokemonSlim } from '@pokemon/types/pokemons';
+import { PokemonCard } from '../pokemon-card/pokemon-card';
+
+export const AllPokemonList = () => {
+  const { data, isLoading, hasNextPage, fetchNextPage } = useInfinitePokemons();
+  const { isIntersecting, ref } = useIsVisible();
+
+  useEffect(() => {
+    if (isIntersecting && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [isIntersecting, hasNextPage, fetchNextPage]);
+
+  return (
+    <div className="pt-32">
+      <section className="flex justify-center">
+        <div className="container px-2 md:px-0 grid gap-8 md-gap-24 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {data?.pages
+            .flatMap((page) => page.results)
+            .map((pokemon: PokemonSlim) => (
+              <PokemonCard pokemon={pokemon} key={pokemon.id} />
+            ))}
+        </div>
+      </section>
+
+      {hasNextPage ? <div ref={ref} className="my-8 h-2" /> : null}
+    </div>
+  );
+};
